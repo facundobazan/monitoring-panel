865930218c9e9863c3b74d16c81aa4330a9f650d
Facundo <facundobazan@icloud.com>
2022-10-03 18:28:16 -0300

    configure firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'monitoring-panel-fd95c',
+    appId: '1:622569723263:web:3e7f4a0e11e83f8493764a',
+    storageBucket: 'monitoring-panel-fd95c.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCaCk8TtjUmPHG6HCUxCUaEbVHkRfSj8FE',
+    authDomain: 'monitoring-panel-fd95c.firebaseapp.com',
+    messagingSenderId: '622569723263',
+  },
   production: true
 };
